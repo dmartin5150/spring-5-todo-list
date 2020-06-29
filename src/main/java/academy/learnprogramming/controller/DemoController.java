e6a9b91ddcf379f56eeef45ea79cec7d7d0fd86b
dmartin5150 <dmartin5150@icloud.com>
2020-06-28 21:08:23 -0500

    Model and Model Attributes

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @Controller
 public class DemoController {
+
+    // fields
+    private final DemoService demoService;
+
+    // constructor
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // request methods
+
 
     // http://localhost:8080/todo-list/hello
     @ResponseBody
@@ -21,17 +35,19 @@ public class DemoController {
     // http://localhost:8080/todo-list/welcome
     @GetMapping("welcome")
     public String welcome(Model model) {
-        model.addAttribute("user", "Tim");
+        model.addAttribute("helloMessage", demoService.getHelloMessage("Tim"));
         log.info("model= {}", model);
         // prefix + name + suffix
         // /WEB-INF/view/welcome.jsp
         return "welcome";
     }
 
+
+    // model attributes
     @ModelAttribute("welcomeMessage")
     public String welcomeMessage() {
         log.info("welcomeMessage() called ");
-        return "Welcome to this Demo application";
+        return demoService.getWelcomeMessage();
     }
 
 
